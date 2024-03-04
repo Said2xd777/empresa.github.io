c75d49b8002264f7fdfa6bf67656b835ecc53c2e
PabloYael <159329463+PabloYael@users.noreply.github.com>
2024-03-04 11:55:25 -0600

    Update funcionesJS.js

--- JavaScript/funcionesJS.js
+++ JavaScript/funcionesJS.js
@@ -1,3 +1,16 @@
+const formulario = document.querySelector('form');
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
 
+    const destinatario = "gnprueba9@gmail.com";
+
+    // Obtener los valores de los campos
+    const username = document.getElementById('nombre').value;
+    const  correo= document.getElementById('email').value;
+    const area = document.getElementById('mensaje').value;
+
+    const enlaceMailto = `mailto:${destinatario}?subject=Mensaje%20de%20${encodeURIComponent(nombre)}&body=${encodeURIComponent(mensaje)}%0D%0A%0D%0A${encodeURIComponent(email)}`;
+    window.location.href = enlaceMailto;
+});
 
   
