ccc2a85fa3da30827c482f4015d63e271fb9b836
PabloYael <159329463+PabloYael@users.noreply.github.com>
2024-03-20 12:04:40 -0600

    Update script1.js

--- script1.js
+++ script1.js
@@ -79,6 +79,8 @@ function eliminarCurso(e)
 //vacias Carrito
 function vaciarcarrito() 
 {
+      miBoton.style.backgroundColor = "skyblue";
+  miBoton.style.color = "white";
     //listaCursos.innerHTML = '';
     while(listaCursos.firstChild){
         listaCursos.removeChild(listaCursos.firstChild);
